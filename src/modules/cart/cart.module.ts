dd5d6b20b2a00221ad833d4f6aecf5c3d06aa4fa
phucmaixuan <phuc.maimanyhued@hcmut.edu.vn>
2026-05-04 17:28:34 +0700

    Refactor CartModule to use FirestoreCartService

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -3,21 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
-import { Cart } from '../../entities/cart.entity';
-import { CartItem } from '../../entities/cart-item.entity';
 import { Product } from '../../entities/product.entity';
 import { User } from '../../entities/user.entity';
 import { VouchersModule } from '../vouchers/vouchers.module';
 import { Neo4jService } from '../../config/neo4j.config';
+import { FirebaseConfig } from '../../config/firebase.config';
+import { FirestoreCartService } from './firestore-cart.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Cart, CartItem, Product, User]),
+    TypeOrmModule.forFeature([Product, User]),
     PassportModule,
-    VouchersModule, // Import VouchersModule to use VouchersService
+    VouchersModule,
   ],
   controllers: [CartController],
-  providers: [CartService, Neo4jService],
+  providers: [CartService, FirestoreCartService, FirebaseConfig, Neo4jService],
   exports: [CartService],
 })
 export class CartModule {}
